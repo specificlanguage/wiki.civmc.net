05e6b605ad893c7002647d416b2de6dacff46c9a
RedDevel2 <61326916+RedDevel2@users.noreply.github.com>
2023-09-18 16:41:31 +0200

    Update sidebar

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -17,12 +17,32 @@ export default defineConfig({
 
     sidebar: [
       { text: 'Getting Started', link: '/pages/getting-started'},
+      { text: 'Change Log', link: '/pages/changelog'},
       {
         text: 'Plugins',
         items: [
           { text: 'Essential Plugins', link: 'pages/plugins/essential/', collapsed: true, items: [
-            { text: 'ExilePearl', link: 'pages/plugins/essential/exilepearl' },
             { text: 'NameLayer', link: 'pages/plugins/essential/namelayer' },
+            { text: 'Citadel', link: 'pages/plugins/essential/citadel' },
+            { text: 'ExilePearl', link: 'pages/plugins/essential/exilepearl' },
+          ]},
+          { text: 'Unique Mechanics', link: 'pages/plugins/unique/', collapsed: true, items: [
+            { text: 'Hiddenore', link: 'pages/plugins/unique/hiddenore' },
+            { text: 'Factorymod', link: 'pages/plugins/unique/factorymod' },
+            { text: 'Finale', link: 'pages/plugins/unique/finale' },
+            { text: 'JukeAlert', link: 'pages/plugins/unique/jukealert' },
+            { text: 'Bastions', link: 'pages/plugins/unique/bastions' },
+            { text: 'Realistic Biomes', link: 'pages/plugins/unique/rb' },
+          ]},
+          { text: 'Fun', link: 'pages/plugins/fun/', collapsed: true, items: [
+            { text: 'Item Exchange', link: 'pages/plugins/fun/itemexchange' },
+            { text: 'Brewery', link: 'pages/plugins/fun/brewery' },
+            { text: 'Wordbank', link: 'pages/plugins/fun/wordbank' },
+            { text: 'Railswitch', link: 'pages/plugins/fun/railswitch' },
+            { text: 'Elevators', link: 'pages/plugins/fun/elevators'},
+            { text: 'Castlegate', link: 'pages/plugins/fun/castlegate' },
+            { text: 'Arthropod Egg', link: 'pages/plugins/fun/arthropodegg' },
+            { text: 'Chunk Limits', link: 'pages/plugins/fun/chunklimits' },
           ]},
         ]
       }
